abe61e9beb536c6c45333fcc2f5f7aac22981223
lidxgoesgiri <gusgirik@gmail.com>
2025-12-29 19:38:43 +0700

    Implementasi Role-Based Access Control (RBAC)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
@@ -19,7 +20,10 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        if (Auth::user()->role !== 'admin') {
+            abort(403, 'Unauthorized action.');
+        }
+        return view('products.create');
     }
 
     /**
--- database/migrations/2025_12_29_121949_add_role_to_users_table.php
+++ database/migrations/2025_12_29_121949_add_role_to_users_table.php
@@ -12,17 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('role')->default('user');
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 };
